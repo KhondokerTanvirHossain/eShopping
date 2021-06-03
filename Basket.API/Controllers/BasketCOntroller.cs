546c6dd4973a8dedf8190e563f4c3a6256716f5b
K. Tanvir Hossain <tanvir@cokreates.com>
2021-06-03 09:00:51 +0600

    add basket controller

--- Basket.API/Controllers/BasketCOntroller.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace Basket.API.Controllers
-{
-    public class BasketController : ControllerBase
-    {
-        
-    }
-}
---
+++ Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Basket.API.Database.Entities;
+using Basket.API.Database.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+        
+        [HttpGet]
+        [ProducesResponseType(typeof(BasketCart), (int) HttpStatusCode.OK)]
+        public async Task<ActionResult<BasketCart>> GetBasket(string userName)
+        {
+            return Ok(await _repository
+                .GetBasket(userName)
+            );
+        }
+        
+        [HttpPost]
+        [ProducesResponseType(typeof(BasketCart), (int) HttpStatusCode.OK)]
+        public async Task<ActionResult<BasketCart>> UpdateBasket(BasketCart basket)
+        {
+            return Ok(await _repository
+                .UpdateBasket(basket)
+            );
+        }
+        
+        [HttpDelete]
+        [Route("{userName}")]
+        [ProducesResponseType(typeof(bool), (int) HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> DeleteBasket(string userName)
+        {
+            return Ok(await _repository
+                .DeleteBasket(userName)
+            );
+        }
+    }
+}
